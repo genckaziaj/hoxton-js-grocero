4c66900847080bf339954237484da1d4678b11ee
Genci23 <genckaziaj@yahoo.com>
2022-06-29 01:59:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+/*
+Description
+In this exercise we explore a common scenario in eCommerce, adding and removing items from the cart, and calculating the total.
+
+Deliverables
+- A user can view a selection of items in the store
+- From the store, a user can add an item to their cart
+- From the cart, a user can view and adjust the number of items in their cart
+- If an item's quantity equals zero it is removed from the cart
+- A user can view the current total in their cart
+
+Instructions
+- Use this template as a starting point => https://codesandbox.io/s/day-11-grocero-template-j2yis
+- Create a state object
+- Create action functions that update state
+- Create render functions  that read from state and update the visuals
+*/
+
+let state = {
+  storeItems: [
+    {
+      id: 1,
+      name: "beetroot",
+      price: 0.45,
+      stock: 10,
+      inCart: 0,
+    },
+    {
+      id: 2,
+      name: "carrot",
+      price: 0.15,
+      stock: 2,
+      inCart: 5,
+    },
+    {
+      id: 3,
+      name: "apple",
+      price: 0.25,
+      stock: 1,
+      inCart: 0,
+    },
+  ],
+};
+
+// input: item
+// action: generates a path for this item's image
+// output: the path
+function getItemImagePath(item) {
+  let id = String(item.id).padStart(3, "0");
+  return `assets/icons/${id}-${item.name}.svg`;
+}
+
+// input: nothing
+// action: get the items that are in the cart
+// output: the cart items
+function getCartItems() {
+  return state.storeItems.filter((item) => item.inCart > 0);
+}
+
+// output: the current total
+function getTotal() {}
+
+function increaseQuantity(item) {
+  if (item.stock === 0) return;
+
+  item.inCart++;
+  item.stock--;
+}
+
+function decreaseQuantity(item) {
+  if (item.inCart > 0) {
+    item.inCart--;
+    item.stock++;
+  }
+}
+
+function renderStoreItems() {
+  let storeUl = document.querySelector(".store--item-list");
+  storeUl.textContent = "";
+
+  for (let item of state.storeItems) {
+    let storeItemEl = document.createElement("li");
+
+    let iconDiv = document.createElement("div");
+    iconDiv.className = ".store--item-icon";
+
+    let iconImg = document.createElement("img");
+    iconImg.src = getItemImagePath(item);
+
+    let addBtn = document.createElement("button");
+    addBtn.textContent = `Add to cart (${item.stock})`;
+    addBtn.addEventListener("click", function () {
+      increaseQuantity(item);
+      render();
+    });
+
+    iconDiv.append(iconImg);
+    storeItemEl.append(iconDiv, addBtn);
+    storeUl.append(storeItemEl);
+  }
+}
+
+function renderCartItems() {
+  let cartUl = document.querySelector(".cart--item-list");
+  cartUl.textContent = "";
+
+  let cartItems = getCartItems();
+
+  for (let item of cartItems) {
+    let cartLi = document.createElement("li");
+
+    let itemImg = document.createElement("img");
+    itemImg.className = "cart--item-icon";
+    itemImg.src = getItemImagePath(item);
+    itemImg.alt = item.name;
+
+    let itemNameP = document.createElement("p");
+    itemNameP.textContent = item.name;
+
+    let removeBtn = document.createElement("button");
+    removeBtn.className = "quantity-btn remove-btn center";
+    removeBtn.textContent = "-";
+    removeBtn.addEventListener("click", function () {
+      decreaseQuantity(item);
+      render();
+    });
+
+    let quantitySpan = document.createElement("span");
+    quantitySpan.className = "quantity-text center";
+    quantitySpan.textContent = String(item.inCart);
+
+    let addBtn = document.createElement("button");
+    addBtn.className = "quantity-btn add-btn center";
+    addBtn.textContent = "+";
+    addBtn.addEventListener("click", function () {
+      increaseQuantity(item);
+      render();
+    });
+
+    cartLi.append(itemImg, itemNameP, removeBtn, quantitySpan, addBtn);
+    cartUl.append(cartLi);
+  }
+}
+
+function renderTotal() {
+  // update the total text
+}
+
+function render() {
+  renderStoreItems();
+  renderCartItems();
+  renderTotal();
+}
+
+render();
